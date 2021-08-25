af9fa4c795c70b2bf723c07e3cefdcff5f7fd719
NicolaRebecca <master9895@gmail.com>
2021-08-25 18:39:35 +0200

    ex. done

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<main>
+
+<?php
+
+$loremText = "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>";
+echo $loremText; 
+
+$lunghezza = strlen($loremText);
+echo "<br>La stringa ha {$lunghezza} caratteri<br/>";
+
+?>
+
+<?php 
+
+
+
+$testo = "<br>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>";
+$censura = "***"; 
+$badwords = array("Lorem");
+foreach($badwords as $chiave){ 
+$msg = str_replace($chiave, $censura, $testo); 
+} 
+echo $msg; 
+
+$lunghezza = strlen($testo);
+echo "<br>La stringa ha {$lunghezza} caratteri<br/>";
+
+?>
+
+
+</main>
+
+</body>
+</html>
